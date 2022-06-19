f423ed0cd05dc615da065420d34a758ef3c551eb
Tata Vishnu Suhaas <vishnusuhaas.thaha@gmail.com>
2022-06-19 18:35:18 +0530

    rectified view engine mistake in app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const authRoutes = require("./routes/auth.routes");
 
 const app = express();
 
-app.set("view-engine","ejs");
+app.set("view engine","ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(authRoutes);
