b59db385b3329682993752e98990cb1430e30fc9
Tata Vishnu Suhaas <vishnusuhaas.thaha@gmail.com>
2022-06-19 23:36:14 +0530

    Code to establish connection to a database written

---
+++ data/database.js
@@ -0,0 +1,24 @@
+const mongodb = require("mongodb");
+
+const MongoClient = mongodb.MongoClient;
+
+let database;
+
+async function connectToDatabase() {
+  const client = await MongoClient.connect("mongodb://127.0.0.1:27017");
+  database = client.db("online-shop")
+}
+
+function getDb(){
+    if(!database){
+        throw new Error("You must connect to a database first");
+    }
+    return database
+}
+
+module.exports={
+    connectToDatabase: connectToDatabase,
+    getDb: getDb,
+}
+
+
