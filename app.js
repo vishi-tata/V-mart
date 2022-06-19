f943f83dae7036e1c9f0ad79b51855a9f4b714d3
Tata Vishnu Suhaas <vishnusuhaas.thaha@gmail.com>
2022-06-19 23:33:17 +0530

    Added database connection to app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 
+const db = require("./data/database")
 const authRoutes = require("./routes/auth.routes");
 
 const app = express();
@@ -12,4 +13,9 @@ app.use(express.static("public"));
 
 app.use(authRoutes);
 
-app.listen(3000);
+db.connectToDatabase().then(function(){
+    app.listen(3000);
+}).catch(function(error){
+    console.log("Failed to connect to database!");
+    console.log(error)
+});
