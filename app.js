03ca579f4b79b15467fc08dfd3694cf4535e1063
Tata Vishnu Suhaas <vishnusuhaas.thaha@gmail.com>
2022-06-20 20:33:24 +0530

    new routes and auth-checking middleware added

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const express = require("express");
 const csrf = require("csurf");
 const expressSession = require("express-session");
 
-const db = require("./data/database")
+const db = require("./data/database");
+const baseRoutes = require("./routes/base.routes");
 const authRoutes = require("./routes/auth.routes");
+const productsRoutes = require("./routes/products.routes");
+const checkAuthStatusMiddleware = require("./middlewares/check-auth");
 const csrfTokenMiddleware = require("./middlewares/csrf-token");
 const errorHandlerMiddleware = require("./middlewares/error-handler");
 const createSessionConfig = require("./config/session");
@@ -24,8 +27,11 @@ app.use(expressSession(sessionConfig));
 app.use(csrf());
 
 app.use(csrfTokenMiddleware);
+app.use(checkAuthStatusMiddleware);
 
+app.use(baseRoutes);
 app.use(authRoutes);
+app.use(productsRoutes);
 
 app.use(errorHandlerMiddleware);
 
