8b523c6d632ef6b046f3525db2bfb321edc82a9e
Tata Vishnu Suhaas <vishnusuhaas.thaha@gmail.com>
2022-06-20 22:38:55 +0530

    server side validation of user input data for signup created

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,11 +1,26 @@
 const User = require("../models/user.model");
 const authUtil = require("../util/authentication");
+const validation = require("../util/validation");
 
 function getSignUp(req, res) {
   res.render("customer/auth/signup");
 }
 
-async function signUp(req, res) {
+async function signUp(req, res, next) {
+  if (
+    !validation.userDetailsAreValid(
+      req.body.email,
+      req.body.password,
+      req.body.name,
+      req.body.street,
+      req.body.postal,
+      req.body.city
+    ) ||
+    !validation.emailIsConfirmed(req.body.email, req.body["confirm-email"])
+  ) {
+    return res.redirect("/signup");
+  }
+
   const user = new User(
     req.body.email,
     req.body.password,
@@ -14,39 +29,52 @@ async function signUp(req, res) {
     req.body.postal,
     req.body.city
   );
-  await user.signUp();
+
+  try {
+    const existsAlready = await user.existsAlready();
+    if (existsAlready) {
+      return res.redirect("/signup");
+    }
+    await user.signUp();
+  } catch (error) {
+    return next(error);
+  }
 
   res.redirect("/login");
-
 }
 
 function getLogin(req, res) {
   res.render("customer/auth/login");
 }
 
-async function login(req,res){
-  const user = new User(req.body.email,req.body.password);
-  const existingUser = await user.getUserWithSameEmail(); 
-
-  if(!existingUser){
+async function login(req, res, next) {
+  const user = new User(req.body.email, req.body.password);
+  let existingUser;
+  try {
+    existingUser = await user.getUserWithSameEmail();
+  } catch (error) {
+    return next(error);
+  }
+  if (!existingUser) {
     res.redirect("/login");
     return;
   }
 
-  const passwordIsMatching = await user.hasMatchingPassword(existingUser.password);
+  const passwordIsMatching = await user.hasMatchingPassword(
+    existingUser.password
+  );
 
-  if(!passwordIsMatching){
+  if (!passwordIsMatching) {
     res.redirect("/login");
     return;
   }
 
-  authUtil.createUserSession(req,existingUser,function(){
+  authUtil.createUserSession(req, existingUser, function () {
     res.redirect("/");
   });
-
 }
 
-function logOut(req,res){
+function logOut(req, res) {
   authUtil.destroyUserAuthSession(req);
   res.redirect("/");
 }
--- models/user.model.js
+++ models/user.model.js
@@ -28,6 +28,14 @@ class User {
     return db.getDb().collection("users").findOne({email: this.email});
   }
 
+  async existsAlready(){
+    const existingUser = await this.getUserWithSameEmail();
+    if(existingUser){
+      return true;
+    }
+    return false;
+  }
+
   hasMatchingPassword(hashedPassword){
     return bcrypt.compare(this.password,hashedPassword);
   }
