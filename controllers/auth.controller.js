50d18a084fbcdcd4264d1c8686dab41309ad15b9
Tata Vishnu Suhaas <vishnusuhaas.thaha@gmail.com>
2022-06-20 11:24:41 +0530

    Added functionality for signup and login methods in controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,10 +1,30 @@
+const User = require("../models/user.model");
+
 function getSignUp(req, res) {
-    res.render("customer/auth/signup");
+  res.render("customer/auth/signup");
 }
 
-function getLogin(req, res) {}
+async function signUp(req, res) {
+  const user = new User(
+    req.body.email,
+    req.body.password,
+    req.body.name,
+    req.body.street,
+    req.body.postal,
+    req.body.city
+  );
+  await user.signUp();
+
+  res.redirect("/login");
+
+}
+
+function getLogin(req, res) {
+  res.render("customer/auth/login");
+}
 
 module.exports = {
   getSignUp: getSignUp,
   getLogin: getLogin,
+  signUp: signUp,
 };
