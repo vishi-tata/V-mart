8aa0d092eb04c2ff1124a653a64b537c9bb78215
Tata Vishnu Suhaas <vishnusuhaas.thaha@gmail.com>
2022-06-20 13:05:45 +0530

    Error handling and session middleware added to app.js

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-const express = require("express");
 const path = require("path");
+
+const express = require("express");
 const csrf = require("csurf");
+const expressSession = require("express-session");
 
 const db = require("./data/database")
 const authRoutes = require("./routes/auth.routes");
 const csrfTokenMiddleware = require("./middlewares/csrf-token");
+const errorHandlerMiddleware = require("./middlewares/error-handler");
+const createSessionConfig = require("./config/session");
 
 const app = express();
 
@@ -14,11 +18,16 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: false}));
 
+const sessionConfig = createSessionConfig();
+
+app.use(expressSession(sessionConfig));
 app.use(csrf());
 
 app.use(csrfTokenMiddleware);
 
 app.use(authRoutes);
+
+app.use(errorHandlerMiddleware);
 
 db.connectToDatabase().then(function(){
     app.listen(3000);
