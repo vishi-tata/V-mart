8b91c506ad11ef6559a92bf2b120e99526985ff7
Tata Vishnu Suhaas <vishnusuhaas.thaha@gmail.com>
2022-06-20 11:20:36 +0530

    Added middleware to parse incoming data

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.set("view engine","ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static("public"));
+app.use(express.urlencoded({extended: false}));
 
 app.use(authRoutes);
 
