346011da78534f3a656b912321a05a5e126bd797
Tata Vishnu Suhaas <vishnusuhaas.thaha@gmail.com>
2022-06-26 16:11:50 +0530

    Payment mechanism added via stripe package

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -1,36 +1,65 @@
+const stripe = require('stripe')('sk_test_51LEdb1SBLWDdSAXhoTVlDUFnZsE3zAwWjTCnnT5k9gfKRe1hlHbRdrLRZSWa5FTXR56RRH671vyPvj4c2t4jvXLN00YXVkSeIJ')
+
 const Order = require("../models/order.model");
 const User = require("../models/user.model");
 
-async function getOrders(req,res){
+async function getOrders(req, res) {
     const orders = await Order.findAllForUser(res.locals.uid);
-    res.render("customer/orders/all-orders",{orders:orders});
+    res.render("customer/orders/all-orders", { orders: orders });
 }
 
-async function addOrder(req,res,next){
+async function addOrder(req, res, next) {
     const cart = res.locals.cart;
     let userData
-    try{
+    try {
         userData = await User.findUserById(res.locals.uid);
 
-    } catch(error){
+    } catch (error) {
         next(error);
         return;
     }
-    const order = new Order(cart,userData);
+    const order = new Order(cart, userData);
 
-    try{
+    try {
         await order.save();
-    } catch(error){
+    } catch (error) {
         return next(error);
     }
 
     req.session.cart = null;
 
-    res.redirect("/orders");
+    const session = await stripe.checkout.sessions.create({
+        line_items: cart.items.map(function(item) {
+            return {
+                price_data: {
+                    currency: "inr",
+                    product_data: {
+                        name: item.product.title,
+                    },
+                    unit_amount: +item.product.price.toFixed(2)*100,
+                },
+                quantity: item.quantity,
+            }
+        }),
+        mode: 'payment',
+        success_url: `http://localhost:3000/orders/success`,
+        cancel_url: `http://localhost:3000/orders/failure`,
+    });
 
+    res.redirect(303, session.url);
+}
+
+function getSuccess(req,res){
+    res.render("customer/orders/success");
+}
+
+function getFailure(req,res){
+    res.render("customer/orders/success");
 }
 
 module.exports = {
     getOrders: getOrders,
     addOrder: addOrder,
+    getSuccess: getSuccess,
+    getFailure: getFailure,
 }
