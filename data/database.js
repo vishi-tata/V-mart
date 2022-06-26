4c8ac8d49c664655e4e98ae5b6bdb9a42b1a37a2
Tata Vishnu Suhaas <vishnusuhaas.thaha@gmail.com>
2022-06-26 20:06:58 +0530

    Deploy

--- data/database.js
+++ data/database.js
@@ -3,11 +3,14 @@ const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 
 let database;
+
 let mongodbUrl = 'mongodb://localhost:27017';
 
 if(process.env.MONGODB_URL){
   mongodbUrl = process.env.MONGODB_URL;
 }
+
+console.log(mongodbUrl);
 
 async function connectToDatabase() {
   const client = await MongoClient.connect(mongodbUrl);
